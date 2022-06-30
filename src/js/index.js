fc194b226f4caa9310e11ba9f66ca772c4bf701b
Vsord <stanislav060977@gmail.com>
2022-06-30 16:16:04 -0400

    this-added

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,26 @@
 import '../styles/main.scss';
 
 
-function changeObj(obj) {
-
-    delete obj.age
-    delete obj.email
-
-    if (obj.size) {
-     obj.size += 'cm'
-    }  
-    if (obj.weight) {
-     obj.weight += 'kg'
-    }
-    console.log(obj);
-};
-
-changeObj({fn: 'Lisa', ln: 'Müller', age: 17, size: 175, weight: 67});
-changeObj({fn: 'Martin', ln: 'Harper', age: 26, email: 'martin.harper@test.de', weight: 102});
-changeObj({fn: 'Andrew', ln: 'Harper', age: 81, size: 175, weight: 71});
-changeObj({fn: 'Matthew', ln: 'Müller', age: 19, email: 'matthew@mueller.de'});
+var person1 = {fn: 'Lisa', ln: 'Müller', age: 17, size: 175, weight: 67};
+var person2 = {fn: 'Martin', ln: 'Harper', age: 26, email: 'martin.harper@test.de', weight: 102};
+var person3 = {fn: 'Andrew', ln: 'Harper', age: 81, size: 175, weight: 71};
+var person4 = {fn: 'Matthew', ln: 'Müller', age: 19, email: 'matthew@mueller.de'};
 
 
-
-
-
-
-
-
+function changeObj() {
+     delete this.age
+     delete this.email
+ 
+     if (this.size) {
+          this.size += 'cm'
+     }  
+     if (this.weight) {
+          this.weight += 'kg'
+     }
+     console.log(this);
+ };
+ 
+ changeObj.call(person1);
+ changeObj.call(person2);
+ changeObj.call(person3);
+ changeObj.call(person4);
