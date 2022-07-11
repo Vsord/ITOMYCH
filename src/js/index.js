b507a3f043ad734465fd812fd0c82ee6f137c827
Vsord <stanislav060977@gmail.com>
2022-07-11 17:54:13 -0400

    xhr-promise-async-added

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,69 @@
 import '../styles/main.scss';
 
 
-class Component {
-    constructor(options) {
-        this.element = document.getElementById(options.selector);
-    };
+// Promise //////////////////////////////////////////////////////////////////
 
-    get showID() {
-        return this.element;
+function requestData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(res => { return res.json(); })
+            .then(data => { console.log(data.filter(obj => obj.id <= 10)); })
+            resolve();
+    })
+    .then(() => {
+        console.log('Data received');
+    })
+};
+requestData('https://jsonplaceholder.typicode.com/posts');
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Async / Await ////////////////////////////////////////////////////////////
+
+async function filterData(url) {
+    try {
+        const response = await fetch(url);
+        const getData = await response.json();
+        console.log(getData.filter(obj => obj.id <= 10));
+    } catch(err) {
+        console.error(err);
     }
-
-    set changeID(value) {
-        this.element = document.getElementById(value);
-    };
 };
+filterData('https://jsonplaceholder.typicode.com/posts');
 
-class Block extends Component {
 
-    constructor(options) {
-        super(options)
-        this.element.style.width = options.width + 'px';
-        this.element.style.height = options.height + 'px';
-        this.element.style.backgroundColor = options.color;
-        this.element.style.display = options.visible;
-    };
+/////////////////////////////////////////////////////////////////////////////
 
-    showElem() {
-        return this.element.style.display = 'block';
-    };
 
-    hideElem() {
-        return this.element.style.display = 'none';
-    };
 
-    changeWidth = (newWidth) => {
-        return this.element.style.width = newWidth + 'px';
-    };
+// Чисто ради прикола ///////////////////////////////////////////////////////
 
-    changeHeight = (newHeight) => {
-        return this.element.style.height = newHeight + 'px';
-     };
+const url = 'https://jsonplaceholder.typicode.com/posts';
+const xhr = new XMLHttpRequest();
 
-    changeColor = (color) => {
-        return this.element.style.backgroundColor = color;
-    };
+const paragraph = document.getElementById('paragraph');
+const title = document.getElementById('title');
+
+
+
+xhr.open('GET', url);
+xhr.responseType = 'json';
+
+xhr.onload = () => {
+
+    title.style.color = 'red';
+    let titles = xhr.response[0].title;
+    title.innerHTML = titles;
+
+
+    let elems = xhr.response[0].body;
+    paragraph.innerHTML = elems;
 
 };
 
-var box = new Block({
-    selector: 'block',
-    width: 100,
-    height: 100,
-    color: 'green',
-    visible: 'none',
-});
+xhr.send();
 
+/////////////////////////////////////////////////////////////////////////////
 
 
 
