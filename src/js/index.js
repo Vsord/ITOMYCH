8f51ddb353930f575133f00c943f564983a58023
Vsord <stanislav060977@gmail.com>
2022-07-21 19:46:22 -0400

    function3 changed

--- src/js/index.js
+++ src/js/index.js
@@ -3,50 +3,58 @@ import '../styles/main.scss';
 
 // Function 1 //////////////////////////////////////////////////////
 
-// let sortedArray = arr => arr.sort();
+let sortedArray = arr => arr.sort();
 
-// console.log(sortedArray(["b","c","d","a"]));
-// console.log(sortedArray(["z", "c", "d", "a", "y", "a", "w"]));
+console.log(sortedArray(["b","c","d","a"]));
+console.log(sortedArray(["z", "c", "d", "a", "y", "a", "w"]));
 
 ////////////////////////////////////////////////////////////////////
 
 
 // Function 2 //////////////////////////////////////////////////////
 
-// function argumentsSum(a, b) {
-//     var reducer = a.reduce((sum, currentValue) => {
-//             if (currentValue > b) {
-//                 return sum + currentValue 
-//             } else {
-//                 return sum
-//             }
-//     }, 0);
-//     return reducer
-// };
+function argumentsSum(a, b) {
+    var reducer = a.reduce((sum, currentValue) => {
+            if (currentValue > b) {
+                return sum + currentValue 
+            } else {
+                return sum
+            }
+    }, 0);
+    return reducer
+};
 
-// console.log(argumentsSum([1, 2, 3, 4, 5, 6, 7], 2));
-// console.log(argumentsSum([-10, -11, -3, 1, -4], -3));
-// console.log(argumentsSum([78, 99, 100, 101, 401], 99));
+console.log(argumentsSum([1, 2, 3, 4, 5, 6, 7], 2));
+console.log(argumentsSum([-10, -11, -3, 1, -4], -3));
+console.log(argumentsSum([78, 99, 100, 101, 401], 99));
 
 ////////////////////////////////////////////////////////////////////
 
 // Function 3 //////////////////////////////////////////////////////
 
-// function getUnique(a, b) {
-//     var newArray = [];
-//     var merged = a.concat(b);
-//     merged.forEach((item, index, merged) => {
-//         if (merged.indexOf(item) == index) {
-//             newArray.push(item)
-//         }
-//     });
-//     return newArray.sort((a, b) => a - b);
-// };
+function getUnique(a, b) {
+    return a.concat(b).filter((item, index, merged) => {
+        if (merged.indexOf(item) == index) {
+            return item
+        }
+    }).sort((a, b) => a - b);
+};
 
-// console.log(getUnique([1, 2, 3], [3, 4, 5]));
-// console.log(getUnique([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
+console.log(getUnique([1, 2, 3], [3, 4, 5]));
+console.log(getUnique([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
 
 ////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
 
 
