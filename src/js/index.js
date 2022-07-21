b40e624ef8bb920d1b0bf859c6ae6ba2e8c421f1
Vsord <stanislav060977@gmail.com>
2022-07-21 18:16:47 -0400

    abstraction changed

--- src/js/index.js
+++ src/js/index.js
@@ -3,68 +3,39 @@ import '../styles/main.scss';
 
 // Функция конструктор //////////////////////
 
-// function Car(carBrand, publishYear, hasPowerfullEngine , color, isAbleToRun,) {
-//     this.carBrand = carBrand;
-//     this.publishYear = publishYear;
-//     this.hasPowerfullEngine = hasPowerfullEngine ;
-//     this.color = color
-//     this.isAbleToRun = isAbleToRun;
+function Car(options) {
+    this.carBrand = options.carBrand;
+    this.publishYear = options.publishYear;
+    this.hasPowerfullEngine = options.hasPowerfullEngine;
+    this.color = options.color
+    this.isAbleToRun = options.isAbleToRun;
 
-//     this.start = function() {
-//           return `This ${this.carBrand} is able to run: ${this.isAbleToRun}`;
-//     };
+    this.start = function () {
+        return `This ${this.carBrand} is able to run: ${this.isAbleToRun}`;
+    };
 
-//     this.stop = function() {
-//           return 'Car has been stopped!';
-//     };
+    this.stop = function () {
+        return 'Car has been stopped!';
+    };
 
-//     this.changeColor = function() {
-//           this.color = 'red';
-//           return this.color;
-//     };
+    this.changeColor = function (newColor) {
+        if (newColor) {
+            return this.color = newColor
+        } else if (!newColor) {
+            return this.color
+        }
+    };
 
-// };
+};
 
-// let newCar = new Car('Toyota', 2001, false, 'green', true,);
-// console.log(newCar);
-
-
-
-/////////////////////////////////////////////////////////
-
-// Находит элемент в html,добавляет ему ширину,высоту и меняет цвет ///
-
-function Component(object) {
-     this.element = document.querySelector(object.selector)
-     this.width = object.width;
-     this.height = object.height;
-     this.color = object.color;
-     
-     this.addWidth = () => {
-         return this.element.style.width = this.width + 'px';
-     };
-
-     this.addHeight = () => {
-          return this.element.style.height = this.height + 'px';
-      };
-
-     this.changeColor = () => {
-         return this.element.style.backgroundColor = this.color;
-     };
-
- };
- 
- let newComponent = new Component({
-     selector: '#block',
-     width: 100,
-     height: 100,
-     color: 'red',
- });
-
- console.log(newComponent.addWidth());
- console.log(newComponent.changeColor());
- console.log(newComponent.addHeight());
+let newCar = new Car({
+    carBrand: 'Toyota',
+    publishYear: 2001,
+    hasPowerfullEngine: false,
+    color: 'green',
+    isAbleToRun: true,
+});
+console.log(newCar);
 
 
-/////////////////////////////////////////////////////////////////
 
